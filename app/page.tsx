a2cc04e2bef484f968e8319ada6a76ecb7009db4
Vassilios Moulakakis <68458826+vmoulakakis@users.noreply.github.com>
2026-08-17 16:51:11 +0300

    feat: switch homepage to AIgora release UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { V4HomeStage4 } from "@/components/v4-home-stage4";
+import { AIgoraHome } from "@/components/aigora-home";
 
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return <V4HomeStage4 />;
+  return <AIgoraHome />;
 }
